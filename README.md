6777cc03e2297c91fb52d590bde8fa3cd14d2482
Артемьев Максим Викторович <Artemyev_MV@utair.dom>
2017-07-18 16:38:50 +0500

    Trying to speed up find_duplicate function

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # Anti-Duplicator
 
-This script gets the folder name as a parameter and finds duplicate files in this folder and subfolders, after that it prints the list of duplicates files with the same name and size. 
+This script gets the folder name as a parameter and finds duplicate files in this folder and subfolders, after that it prints the list of duplicates files with the same name and size.
+
+# Quickstart
+
+Example of script launch on Linux
+
+```#!bash
+
+$ python duplicates.py <path to directory>
+
+For example you will see.
+
+c:\astra\locale\RU\test2.txt
+c:\astra\locale\test.txt
+c:\astra\placeimage\test2.txt
+c:\astra\test.txt
+c:\astra\profiles\test.txt
+c:\astra\placeimage\test.txt
+
+```
 
 # Project Goals
 
--- duplicates.py
+++ duplicates.py
@@ -1,25 +1,33 @@
 import sys
 import os
+from collections import Counter
 
 
 def find_duplicate(folder):
 
     files_list = []
+    file_names = []
     duplicates_list = []
     for root, dirs, files in os.walk(folder):
         for file in files:
             file_path = os.path.join(root, file)
             files_list.append([file, os.path.getsize(file_path), file_path])
+            file_names.append(file)
+    
+    duplicate_name = [x for x in Counter(file_names) if Counter(file_names)[x] > 1]
+
+    for filename in files_list:
+        if filename[0] in duplicate_name:
+            duplicates_list.append(filename)
+
+    duplicates_list_copy = duplicates_list
 
     for file1 in files_list:
         for file2 in files_list:
             if (file1[0] == file2[0]) and (file1[1] == file2[1]) and (file1[2] != file2[2]):
-                duplicates_list.append(file1[2])
-
-    duplicates_list = set(duplicates_list)
+                pass
     return duplicates_list
-
-
+    
 def print_duplicate_files(duplicates):
     for filename in duplicates:
         print(filename)
@@ -29,6 +37,7 @@ if __name__ == '__main__':
 
     foldername = sys.argv[1]
     if os.path.isdir(foldername):
-        print_duplicate_files((find_duplicate(foldername)))
+        #print_duplicate_files((find_duplicate(foldername)))
+        print(find_duplicate(foldername))
     else:
         print("Неправильный каталог")
